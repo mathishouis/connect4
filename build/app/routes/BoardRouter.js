d54babb7c9570a3a3416ec267187dedcae7c6600
HOUIS Mathis <59845782+kozennnn@users.noreply.github.com>
2021-10-10 22:25:16 +0200

    Delete build directory

--- build/app/routes/BoardRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const components_1 = require("./../components");
-const router = (0, express_1.Router)();
-// @ts-ignore
-router.get('/', components_1.BoardComponent.index);
-router.get('/:column', components_1.BoardComponent.play);
-exports.default = router;
